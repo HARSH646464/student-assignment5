ff530c051db6b784967b2612fa919df6c7c61c74
HARSH646464 <harshtomar4081@gmail.com>
2025-08-30 15:58:54 +0530

    Add files via upload

---
+++ list_assignment.py
@@ -0,0 +1,6 @@
+list=[1,2,3,4,5,6,7,8,9,10]
+extracted_list=list[0:5]
+reversed_extracted_list=extracted_list[::-1]
+print('Original list: ',list)
+print("Extracted first five elements: ",extracted_list)
+print('Reversed extracted elements: ',reversed_extracted_list)
